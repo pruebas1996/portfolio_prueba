7cae21490c50dfd18157c9ee16344a95649a847d
Mario Vega <vegagonzalezmario06@gmail.com>
2021-04-15 16:41:55 -0500

    portfolio updated

---
+++ app.js
@@ -0,0 +1,26 @@
+const hamburger = document.querySelector('.header .nav-bar .nav-list .hamburger');
+const mobile_menu = document.querySelector('.header .nav-bar .nav-list ul');
+const header = document.querySelector('.header');
+const items_menu = document.querySelectorAll('.header .nav-bar .nav-list ul li a');
+
+hamburger.addEventListener('click', ()=> {
+    hamburger.classList.toggle('active');
+    mobile_menu.classList.toggle('active');
+});
+
+document.addEventListener('scroll', () => {
+    var scroll_position = window.scrollY;
+    if(scroll_position > 150) {
+     header.style.backgroundColor = "#29323c";
+       
+    } else {
+        header.style.backgroundColor = "transparent";
+    }
+});
+
+items_menu.forEach((item)=>{
+    item.addEventListener('click',() =>{
+        hamburger.classList.toggle('active');
+        mobile_menu.classList.toggle('active');
+    })
+})
